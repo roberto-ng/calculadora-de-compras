692f40876115f2d2f6ed88c75bf825a09e35422d
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-04-11 12:29:56 -0300

    Adiionar redux-persist

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
 import itensSlice from './itens'
 
+const reducers = combineReducers({
+    itens: itensSlice,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-    reducer: {
-        itens: itensSlice,
-    },
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk],
 });
 
 export default store;
